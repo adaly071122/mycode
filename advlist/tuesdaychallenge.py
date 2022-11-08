70b8a9f44b4da64798f328b3c8a01062ca3bd211
dalyalan@gmail.com <student@bchd.9a5b665f-f0ab-458b-a3e3-cb6050906a76>
2022-11-08 17:16:12 +0000

    join and split

---
+++ advlist/tuesdaychallenge.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def main():
+
+
+    user_firstname = input ("First Name:")
+    user_surname = input ("Surname:")
+    user_number = input ("FAvourite Number:")
+
+    list1 = [(user_firstname), (user_surname), (user_number)]
+
+
+#Output
+
+    print(list1)
+
+
+main()
